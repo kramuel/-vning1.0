8372d865073d2680ded0471e48615821785ebc0f
Samuel Karlström <sam.karlstrm@gmail.com>
2020-10-13 14:45:10 +0200

    test

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Övning1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Samuel Karlström";
+            string street = "Eknäsgatan 1A";
+            string zip_city = "603 54 Norrköping"
+
+
+        }
+    }
+}
